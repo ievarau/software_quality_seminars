90d4e023eab73d15fbeed3f66750bc62e01ddad8
Paul Boddie <paul.boddie@ncmm.uio.no>
2021-11-12 13:46:38 +0100

    Paul says hello!

---
+++ hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2021 University of Oslo
+# SPDX-FileContributor: Paul Boddie <paul.boddie@ncmm.uio.no>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+print("Hello from Paul!")
+
+# vim: tabstop=4 expandtab shiftwidth=4
