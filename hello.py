30f4dd6ad03bf1b947f5e4bf9560208a0ab721d7
ievarau <ievarau@uio.no>
2022-01-13 15:06:50 +0100

    GIT workshop files organized into a folder.

--- hello.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-# SPDX-FileCopyrightText: 2021 University of Oslo
-# SPDX-FileContributor: Paul Boddie <paul.boddie@ncmm.uio.no>
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-print("Hello from Paul!")
-
-# vim: tabstop=4 expandtab shiftwidth=4
--- ievas_script.R
+++
@@ -1,4 +0,0 @@
-## The beginning of the script
-
-library(tidyverse)
-library(data.table)
