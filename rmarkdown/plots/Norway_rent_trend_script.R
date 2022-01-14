37f7a2aa26a9945c879f6540b90c8f413d7b8806
princeps091-binf <princeps091@gmail.com>
2022-01-14 12:52:28 +0100

    minimal comment

--- rmarkdown/plots/Norway_rent_trend_script.R
+++ rmarkdown/plots/Norway_rent_trend_script.R
@@ -6,8 +6,9 @@ delim = "\t", escape_double = FALSE,
 trim_ws = TRUE)
 
 gg_rent<-Norway_rent %>% 
-  
+  # manual/crafty recoding for category labels
   mutate(Locations=c("Norway","Oslo and Bærum","Akershus except Bærum","Bergen","Trondheim","Stavanger",">20k","2000-19999","<1,999"))%>% 
+  # Convenient reshaping for ploting
   pivot_longer(cols = where(is.numeric),names_to = "year",values_to = "average.rent") %>%
   ggplot(.,aes(year,average.rent,color=Locations,group=Location))+
   geom_line()+
