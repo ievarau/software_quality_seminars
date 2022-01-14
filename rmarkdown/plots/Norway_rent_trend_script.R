0aab5c930edc37c71d767ae992a6c575f30c5214
princeps091-binf <princeps091@gmail.com>
2022-01-14 12:47:16 +0100

    Vipin's plot about rent in Norway

---
+++ rmarkdown/plots/Norway_rent_trend_script.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+
+library(readr)
+Norway_rent <- read_delim("rmarkdown/plots/Norway_rent.csv",
+delim = "\t", escape_double = FALSE,
+trim_ws = TRUE)
+
+gg_rent<-Norway_rent %>% 
+  
+  mutate(Locations=c("Norway","Oslo and Bærum","Akershus except Bærum","Bergen","Trondheim","Stavanger",">20k","2000-19999","<1,999"))%>% 
+  pivot_longer(cols = where(is.numeric),names_to = "year",values_to = "average.rent") %>%
+  ggplot(.,aes(year,average.rent,color=Locations,group=Location))+
+  geom_line()+
+  scale_color_brewer(palette="Paired")+
+  theme(legend.position = "bottom")
+ggsave("./rmarkdown/plots/Norway_rent_trend.png",gg_rent)
